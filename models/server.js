85ff17be065868f87c461b147f27063e936be3e0
Luciano Labudia <labudia.luciano@gmail.com>
2023-04-18 21:12:48 -0300

    CORS - Middleware

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
 
 class Server {
@@ -15,14 +16,45 @@ class Server {
     }
 
     middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
         // Directorio Publico
         this.app.use( express.static('public') );
     }
 
     routes() {
+
         this.app.get('/api', (req, res) => {
-            res.send('Hello World')
-        })
+            res.json({
+                msg: 'get API'
+            })
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                msg: 'put API'
+            })
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                msg: 'post API'
+            })
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                msg: 'delete API'
+            })
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                msg: 'patch API'
+            })
+        });
     }
 
     listen() {
