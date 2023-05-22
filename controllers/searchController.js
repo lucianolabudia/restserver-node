6e56e08893e16671a870ac6babe2f86f4ff2e4fa
Luciano Labudia <labudia.luciano@gmail.com>
2023-05-22 10:45:28 -0300

    Database searches.

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,14 +1,58 @@
-const { response } = require("express");
+const { response } = require('express');
+const { ObjectId } = require('mongoose').Types;
 
+const { User, Category, Product } = require('../models');
+
+
+const permittedCollections = [
+    'users',
+    'category',
+    'products',
+    'roles'
+];
+
+const searchUsers = async( term = '', res = response ) => {
+
+    const isMongoID = ObjectId.isValid( term ); //TRUE
+
+    if ( isMongoID ) {
+        const user = await User.findById( term );
+        res.json({
+            results: ( user ) ? [ user ] : []
+        })
+    }
+}
 
 
 const search = ( req, res = response ) => {
 
     const { collection, term } = req.params
 
-    res.json({
-        collection, term
-    })
+    if ( !permittedCollections.includes( collection ) ) {
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son: ${ permittedCollections }`
+        })
+    }
+
+    switch ( collection ) {
+        case 'users':
+            searchUsers( term, res );
+            break;
+    
+        case 'category':
+            
+            break;
+    
+        case 'products':
+            
+            break;
+    
+        default:
+            res.status(500).json({
+                msg: 'Se le olvido hacer esta busqueda'
+            });
+    }
+
 }
 
 
